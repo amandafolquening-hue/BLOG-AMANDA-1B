c9adaa14e4d99e5bc54b680fad600b22de0afad0
amandafolquening-hue <amanda.folquening@escola.pr.gov.br>
2026-07-06 14:25:44 +0000

    v5

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);{
+
+    function botaoClicado(){
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+    }
+
+}
