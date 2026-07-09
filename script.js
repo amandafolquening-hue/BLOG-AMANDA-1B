0eaa8c0a32c081ccf42814e8cd7a58c6970fe2a1
amandafolquening-hue <amanda.folquening@escola.pr.gov.br>
2026-07-09 15:15:55 +0000

    v7

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const botao = document.querySelector('button');
-botao.addEventListener("click", botaoClicado);
+const botoes = document.querySelectorAll("button");
+botoes.forEach(function (botao) {
+    botao.addEventListener("click", botaoClicado);
 
-    function botaoClicado(){
-        let texto = botao.querySelector('span');
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
         texto.textContent++;
+
     }
 
 
+});
